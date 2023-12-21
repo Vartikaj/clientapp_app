0dd4122fe04cfa00a61d88735f831a37c3c0b1dd
vartikaj <vartikajohari57@gmail.com>
2023-12-21 18:08:36 +0530

    changes

--- src/app/component/outer/registration/registration.component.ts
+++ src/app/component/outer/registration/registration.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistrationComponent implements OnInit {
   registrationForm !: FormGroup;
   submitted = false;
+  hide = true;
+  hideconf = true;
 
   constructor (private formBuilder : FormBuilder) { }
 
@@ -17,16 +19,16 @@ export class RegistrationComponent implements OnInit {
       'fullName': [null, [Validators.required, Validators.pattern('[a-zA-Z .]*')]],
       'username' : [null, [Validators.required, Validators.pattern('[a-zA-Z .]*')]],
       'email' : [null, [Validators.required, Validators.email]],
-      'phoneNumber' : [null, [Validators.required, Validators.pattern('^[0-9]*'), Validators.maxLength(10)]],
+      'phoneNumber' : [null, [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(10)]],
       'password' : [null, [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).+$/), Validators.maxLength(20), Validators.minLength(8)]],
       'confirmPassword' : [null, [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).+$/), Validators.maxLength(20), Validators.minLength(8)]]
+    }, {
+      validator : ConfirmedValidator('password', 'confirmPassword')
+    });
 
-    })
+    this.hide= true;
+    this.hideconf = true;
   }
-
-  // get validators(){
-  //   return this.registrationForm.controls; 
-  // }
 
   submit(){
     this.submitted = true;
@@ -35,7 +37,23 @@ export class RegistrationComponent implements OnInit {
     }
     localStorage.setItem('Persondata', JSON.stringify(this.registrationForm.value));
     alert('SUCCESS!!' + JSON.stringify(this.registrationForm.value,null,4))
-
   }
-
 }
+
+function ConfirmedValidator(controlName: string, matchingControlName: string): any {
+  return (formGroup : FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
+
+    if(matchingControl.errors && !matchingControl.errors['confirmedValidator']) {
+      return;
+    }
+
+    if(control.value !== matchingControl.value) {
+      matchingControl.setErrors({ confirmedValidator : true })
+    } else {
+      matchingControl.setErrors(null);
+    }
+  }
+}
+
